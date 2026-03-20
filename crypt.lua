f9d1851cfdf2db8195f53ab6cbf96ba8c3d67ba9
Dmitry Kotov <53375304+dmitrijkotov634@users.noreply.github.com>
2026-03-20 22:01:14 +0300

    Update crypt.lua

--- crypt.lua
+++ crypt.lua
@@ -356,8 +356,7 @@ end
 
 return function(module)
     module:setCategory "Crypt"
-    module:setDescription "Encryption, encoding, hashing, and password generation toolkit"
-
+    
     module:registerCommand("encrypt", encrypt_cmd, "XOR encrypt: encrypt <key> [text]")
     module:registerCommand("decrypt", decrypt_cmd, "XOR decrypt: decrypt <key> [hex]")
     module:registerCommand("rot13", rot13_cmd, "ROT13 cipher")
